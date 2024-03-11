69e4bc29fd891087e17193c03b23e9da94c35dc0
GabrielSeiji003 <gabrielseiji003@gmail.com>
2024-03-11 13:27:33 -0300

    projeto Inicial

---
+++ app/main.js
@@ -0,0 +1,44 @@
+let livros = []
+const endpointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
+getBuscarLivrosDaAPI()
+
+async function getBuscarLivrosDaAPI() {
+    const res = await fetch(endpointDaAPI)
+    livros = await res.json()
+    let livrosComDesconto = aplicarDesconto(livros)
+    exibirOsLivrosNaTela(livrosComDesconto)
+}
+
+const elementoParaInserirLivros = document.getElementById ('livros')
+const elementoComValorTotalDeLivrosDisponiveis = document.getElementById('valor_total_livros_disponiveis')
+
+function exibirOsLivrosNaTela(listaDeLivros) {
+  elementoComValorTotalDeLivrosDisponiveis.innerHTML = ''
+    elementoParaInserirLivros.innerHTML = ''
+    listaDeLivros.forEach(livro => {
+      // let disponibilidade = verificarDisponibilidadeDoLivro(livro)
+      let disponibilidade = livro.quantidade > 0 ? 'livro__imagens' : 'livro__imagens indisponivel'
+        elementoParaInserirLivros.innerHTML += `
+        <div class="livro">
+      <img class="${disponibilidade}" src="${livro.imagem}"
+        alt="${livro.alt}" />
+      <h2 class="livro__titulo">
+        ${livro.titulo}
+      </h2>
+      <p class="livro__descricao">${livro.autor}</p>
+      <p class="livro__preco" id="preco">R$${livro.preco.toFixed(2)}</p>
+      <div class="tags">
+        <span class="tag">${livro.categoria}</span>
+      </div>
+    </div>
+        `
+    })
+}
+
+// function verificarDisponibilidadeDoLivro(livro) {
+//   if (livro.quantidade > 0){
+//     return 'livro__imagens'
+//   } else {
+//     return 'livros__imagens indisponivel'
+//   }
+// }
---
+++ app/metodoFilter.js
@@ -0,0 +1,29 @@
+const botoes = document.querySelectorAll('.btn')
+botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
+
+function filtrarLivros() {
+    const elementoBTN = document.getElementById(this.id)
+    const categoria = elementoBTN.value
+    let livrosFiltrados = categoria == 'disponivel' ? filtraPorDisponibilidade() : filtrarPorCategoria(categoria)
+    exibirOsLivrosNaTela(livrosFiltrados)
+    if (categoria == 'disponivel') {
+        const valorTotal = calcularValorTotalDeLivrosDisponiveis(livrosFiltrados)
+        exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal)
+    }
+}
+
+function filtrarPorCategoria(categoria) {
+    return livros.filter(livro => livro.categoria == categoria)
+}
+
+function filtraPorDisponibilidade() {
+    return livros.filter(livro => livro.quantidade > 0)
+}
+
+function exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal) {
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML = `
+    <div class="livros__disponiveis">
+      <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
+    </div>
+    `
+}
